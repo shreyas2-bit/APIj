10a161f1b8a2a6187155b866de452d55a4fa744f
shreyas2-bit <62927960+shreyas2-bit@users.noreply.github.com>
2022-01-20 22:26:06 +0530

    persistance using NeDB (building database)

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require('express');
+const Datastore = require('nedb');
+
 const app = express();
 
 app.listen(3000, ()=> console.log('listening at 3000'));
 
 app.use(express.static('public'));
+
+app.use(express.json({limit:'1mb'}));  //for server to recieve data
+
+
+const database = new Datastore('database.db');  
+database.loadDatabase();
+
+app.post('/api', (request, response) => {
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+    console.log(database);
+    response.json({
+        status: 'success',
+        latitude: data.lat,
+        longitude: data.long,
+        timestamp: timestamp
+    });  //function that sends data
+});
